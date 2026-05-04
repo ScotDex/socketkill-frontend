59ef80b7b1d2e9958926b250ca1406984bc8e700
ScotDex <themadlyscientific@gmail.com>
2026-05-04 03:15:53 +0100

    reverting

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-import { resolve } from 'path' // Need this to resolve file paths
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -6,13 +5,4 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about/index.html'), // Path to your about page
-        map: resolve(__dirname, 'map/index.html'),     // Path to your map page
-      },
-    },
-  },
 })
